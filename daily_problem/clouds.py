9106ccb319d63bba47dc1bdf48630a4faf616d7e
Luis Cruz <luisccc99@gmail.com>
2021-04-04 13:58:59 -0600

    finishing jumping on clouds problem

--- daily_problem/clouds.py
+++ daily_problem/clouds.py
@@ -1,10 +1,9 @@
-# TODO: solve problem when index is at penultimate position
 def jumpingOnClouds(clouds):
     steps = 0
     i = 0
     while i < len(clouds) - 1:
         # take 2 steps if it's safe to do so
-        if clouds[i+2] == 0:
+        if (i + 2) < len(clouds) and clouds[i+2] == 0:
             i += 2
             steps += 1
             continue
@@ -15,4 +14,4 @@ def jumpingOnClouds(clouds):
 
 
 if __name__=="__main__":
-    print(jumpingOnClouds([0, 0, 0, 0, 1, 0]))
+    print(jumpingOnClouds([0, 0, 0, 1, 0, 0]))
